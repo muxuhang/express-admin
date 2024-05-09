b0c436c1f035cba70defd5e894a4c98147e61645
muxuhang <17186765121@163.com>
2024-05-09 16:21:23 +0800

    router hash

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express()
 
 // 视图配置
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('view engine', 'html')
 app.engine('html', ejs.renderFile)
 
 // 中间件配置
@@ -26,20 +26,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-// 明确指定静态文件目录
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31536000 }))
-app.use(cors())
-
 // 路由配置
 const routerList = require('./routes/index')
 routerList.map((item) => {
   app.use('/', item)
 })
 
-// 404错误处理
-app.use(function (req, res, next) {
-  const pageNotFoundView = path.join(app.get('views'), '404.html')
-  res.status(404).render(pageNotFoundView)
+// 明确指定静态文件目录
+app.use(express.static(path.join(__dirname, 'views'), { maxAge: 31536000 }))
+app.use(cors())
+
+// 捕获所有未找到的路由，并返回 Vue 应用程序的入口文件
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
 })
 
 // 错误处理
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 import { UsersModel } from '../utils/schema'
-import bcrypt from "bcrypt";
-import { jwtSign } from '../utils/jwt';
+import bcrypt from 'bcrypt'
+import { jwtSign } from '../utils/jwt'
 // 查询参数是否有用户名和密码
 // 查询后台有该用户
 // 匹配密码是否正确
@@ -11,27 +11,26 @@ import { jwtSign } from '../utils/jwt';
 router.post('/api/login', async (req, res, next) => {
   if (!req.body.username) res.status(400).send({ message: '请输入用户名' })
   if (!req.body.password) res.status(400).send({ message: '请输入登录密码' })
-  const count = await UsersModel.find({ username: { $eq: req.body.username } }).count()
+  const count = await UsersModel.find({
+    username: { $eq: req.body.username },
+  }).count()
   if (count < 1) res.status(400).send({ message: '账号不存在' })
   else {
     UsersModel.find({ username: { $eq: req.body.username } }, (err, result) => {
       if (err) {
         res.status(400).send(err)
       } else {
-        bcrypt.compare(
-          req.body.password,
-          result[0].password
-        ).then((r) => {
+        bcrypt.compare(req.body.password, result[0].password).then((r) => {
           if (r) {
             const token = jwtSign({ id: result[0]._id })
             res.send({ access: token })
           } else {
             res.status(400).send({ message: '密码错误' })
           }
-        });
+        })
       }
     })
   }
 })
 
-module.exports = router;
+module.exports = router
